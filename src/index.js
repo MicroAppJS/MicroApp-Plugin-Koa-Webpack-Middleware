335be850addf49426f60e82b21b6d00843c2bce9
zyao89 <zyao89@gmail.com>
2019-09-23 21:28:49 +0800

    :sparkles: 适配版本为 >=0.1.5

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@
 
 module.exports = function KoaWebpackMiddleware(api, opts = {}) {
 
+    api.assertVersion('>=0.1.5');
+
     // commands
     require('./commands/version')(api);
 
-    api.onDevServerMiddleware(({ app, config, options }) => {
-        const onlyNode = options.onlyNode || false;
-        const compiler = options.compiler || false;
-        const devOptions = options.devOptions || {};
-        if (!onlyNode && !!compiler) {
+    api.onDevServerMiddleware(({ app, config, args, compiler, devOptions = {} }) => {
+        if (compiler) {
             const koaWebpackMiddleware = require('./koa-webpack-middleware');
             app.use(koaWebpackMiddleware.devMiddleware(compiler, devOptions));
             app.use(koaWebpackMiddleware.hotMiddleware(compiler, devOptions));
         }
     });
+};
+
+module.exports.configuration = {
+    mode: 'development',
+    description: '针对 Koa 服务的 webpack 开发模式中间件.',
 };
